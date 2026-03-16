4ca0e24c67856bc0bbf065fc29811ed8904a275f
KOEZIGSKOBR4 <>
2026-03-16 10:17:03 +0100

    tangramsteine ausgangsposition implementiert

--- src/main/java/czg/scenes/minigame_scenes/MathematicsLevelScene.java
+++ src/main/java/czg/scenes/minigame_scenes/MathematicsLevelScene.java
@@ -3,23 +3,16 @@ package czg.scenes.minigame_scenes;
 import czg.objects.ButtonObject;
 import czg.objects.Department;
 import czg.objects.DragObject;
+import czg.objects.minigame_objects.TangramPieceObject;
 import czg.util.Images;
 
 public class MathematicsLevelScene extends LevelScene {
     public MathematicsLevelScene(int level) {
         super(Department.MATHEMATICS, level);
-
-        DragObject pieces = new DragObject(Images.get("/assets/minigames/mathematics/tangram_packed.png"), 100, 100);
-
-        pieces.width *= 0.6;
-        pieces.height *= 0.6;
-
-        objects.add(pieces);
         
-        ButtonObject test = new ButtonObject(Images.get("/assets/minigames/mathematics/tangram_packed.png"), 200, 200, () -> {});
+        for(TangramPieceObject piece : TangramPieceObject.PIECES)
+            objects.add(piece);
         
-        test.method = () -> test.rotate(90);
-        
-        objects.add(test);
+        TangramPieceObject.generatePacked(100, 100, 400, 400);
     }
 }
